70e738b1ea949a0ccc83e6550184c70dac0e8b8d
Yael <xyarock@gmail.com>
2023-02-25 11:26:00 -0600

    commit para poder hacer pull

--- pages/index.js
+++ pages/index.js
@@ -9,4 +9,45 @@ return (
   </MainLayout>
 ) 
 
-}
+  
+  }
+
+  export  function Example(){
+    function llamarWallet(){
+      const isPhantom = window.phantom?.solana?.isPhantom;
+  
+      const getProvider= () =>{
+          if('phantom' in window){
+              const provider = window.phantom?.solana;
+  
+              if(provider?.isPhantom){
+                  return provider;
+              }else{
+                  console.log('Algo falló aki');
+              }
+          }
+          window.open('https://phantom.app/','_blank');
+      };
+      const provider = getProvider();
+      
+      try{
+          const resp = window.phantom.solana.connect();
+          console.log(resp.publicKey.toString());
+          console.log('se llamó al fanton we')
+  
+      }catch(err){
+  
+      }
+  
+    }
+   return(
+      <form onSubmit={llamarWallet}>
+        <button type="wallet">wallet</button>
+      </form>
+   );
+  
+  
+  
+  }
+
+
