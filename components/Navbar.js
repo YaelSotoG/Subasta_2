9b8aa1e02e1e06cd17d997c159d08ea6cbf43dbf
Yael <xyarock@gmail.com>
2023-02-26 13:02:27 -0600

    hovver a boton

--- components/Navbar.js
+++ components/Navbar.js
@@ -50,9 +50,11 @@ export default function Navbar() {
   return (
     <>
       <div className="flex justify-end items-center px-3 py-4 bg-gradient-to-b from-black to-white  fixed w-full h-32 z-50 ">
-        <button className="bg-[#8f250c] h-16 rounded-full w-60" onClick={llamarWallet}>
-            <h1 className="text-bold text-4xl text-white">Login</h1>
-        </button>
+      <button class="group relative h-16 w-60 overflow-hidden rounded-lg bg-white text-lg shadow" onClick={llamarWallet}>
+      <div class="absolute inset-0 w-3 bg-[#8f250c] transition-all duration-[250ms] ease-out group-hover:w-full"></div>
+      <span class="relative text-bold text-4xl text-black group-hover:text-white">Login</span>
+      </button>
+     
       </div>
     </>
     
