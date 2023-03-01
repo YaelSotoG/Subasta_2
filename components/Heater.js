43dc6af67acc81691295690e3366ed7477e703c7
Yael Soto <xyarock@gmail.com>
2023-03-01 00:02:20 -0600

    responsive header

--- components/Heater.js
+++ components/Heater.js
@@ -5,10 +5,10 @@
 
 export default function Heater(){
     return (
-<header className="w-full grid grid-cols-2 h-[800px] bg-[#17002E] z-40 ">
+<header className="w-full grid md:grid-cols-2 gric-cols-1 h-[800px] bg-[#17002E] z-40 ">
     
-        <div className="flex items-center justify-center w-full bg-blue-200">
-            <h1 className="text-white text-7xl text-climate">NFeetTies</h1>
+        <div className="flex items-center justify-center w-full ">
+            <h1 className="text-8xl text-climate text-[#FFCB04]">NFeetTies</h1>
         </div>
         <div className="flex items-center justify-center w-full">
             <div className="rotate-45 w-3/4 h-2/4 bg-[#FFCB04]">
